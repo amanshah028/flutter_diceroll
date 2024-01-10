76f2d7315b9f99d6b5f2bf16f135877e12fd27f4
aman <shahaman028@gmail.com>
2024-01-10 22:58:11 +0545

    background color gradiant

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
-void main(){
 
-runApp(const MaterialApp(home: Scaffold(body: Text("RollDice"),),));
+void main() {
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration:const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color.fromARGB(255, 111, 24, 158),
+              Color.fromARGB(255, 172, 60, 242)],
+            ), 
+          ),
+          child: const Center(
+            child: Text('RollDice'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
