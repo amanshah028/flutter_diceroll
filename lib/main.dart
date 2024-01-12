cf47295ea01eabd8e9ced0cb8ce1cc2c995140e2
aman <shahaman028@gmail.com>
2024-01-13 00:06:58 +0545

    1/13

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_application_1/gradiant_container.dart';
+
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradiantContainer( Color.fromARGB(255, 187, 88, 249), Color.fromARGB(255, 98, 13, 144)),
+        body: GradiantContainer(
+          Color.fromARGB(255, 187, 88, 249),
+          Color.fromARGB(255, 98, 13, 144),
+        ),
       ),
     ),
   );
 }
-
